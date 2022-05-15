9b2a334929daa5329171c06c67a2ddfba65c8b86
Leonardo Rojo <leofrojo@gmail.com>
2022-05-15 20:47:55 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const email = document.getElementById("email");
+const btnSend = document.getElementById("submit");
+const error = document.getElementById("error");
+const leyend = document.getElementById("leyend");
+const sent = document.getElementById("sent");
+
+btnSend.addEventListener("click", () => {
+  let validRegex = /\S+@\S+\.\S+/;
+
+  if (email.value.match(validRegex)) {
+    error.classList.remove("warning");
+    leyend.classList.add("leyend-hidden");
+    sent.classList.remove("sent-hidden");
+
+    email.value = "";
+  } else {
+    error.classList.add("warning");
+    leyend.classList.remove("leyend-hidden");
+    sent.classList.add("sent-hidden");
+  }
+});
